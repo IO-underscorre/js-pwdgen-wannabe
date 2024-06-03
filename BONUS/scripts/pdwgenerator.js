f9e8f342fea9074a96206b8a8b01e827c25ea7df
Jacopo Scorretti <jacoposcorretti.99@gmail.com>
2024-06-03 15:19:55 +0200

    rebuild!: random function added and fixed password suffix number made random
    BREAKING CHANGE: recommended password is now composed by first name, last name, favourite color and a random two digits number (in this order), the static two digits number is no more.

--- BONUS/scripts/pdwgenerator.js
+++ BONUS/scripts/pdwgenerator.js
@@ -1,12 +1,17 @@
 window.onload = function () {
+    // Random function
+    function getRandomInt(min, max) {
+        return Math.floor(Math.random() * (max - min) ) + min;
+    }
+
     // Variables initialization and assignation
     let firstName = prompt("Insert your first name");
     let lastName = prompt("Insert your last name");
     let favColor = prompt("Insert your favourite color");
-    const notSoRandomNumber = 23;
+    let randomNumber = getRandomInt(10 , 99);
 
     // Log and print
-    console.log(`${firstName}${lastName}${favColor}${notSoRandomNumber}`)
+    console.log(`${firstName}${lastName}${favColor}${randomNumber}`)
 
-    document.getElementById("password-container").innerHTML = `${firstName}${lastName}${notSoRandomNumber}`;
+    document.getElementById("password-container").innerHTML = `${firstName}${lastName}${randomNumber}`;
 }
