9c286d33a1a341aa7bddd42a22f2d2be0bfd5133
Jacopo Scorretti <jacoposcorretti.99@gmail.com>
2024-06-03 14:56:37 +0200

    build: bonus project initialized

---
+++ BONUS/scripts/pdwgenerator.js
@@ -0,0 +1,10 @@
+// Variables initialization and assignation
+let firstName = prompt("Insert your first name");
+let lastName = prompt("Insert your last name");
+let favColor = prompt("Insert your favourite color");
+const notSoRandomNumber = 23;
+
+// Log and print
+console.log(`${firstName}${lastName}${favColor}${notSoRandomNumber}`)
+
+document.getElementById("password-container").innerHTML = `${firstName}${lastName}${notSoRandomNumber}`;
