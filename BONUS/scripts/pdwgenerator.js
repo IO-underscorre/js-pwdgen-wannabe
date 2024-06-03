826188afa18a04074ded33ec6a74a420b573aef4
Jacopo Scorretti <jacoposcorretti.99@gmail.com>
2024-06-03 15:01:07 +0200

    rebuild: made script start after the page is loaded

--- BONUS/scripts/pdwgenerator.js
+++ BONUS/scripts/pdwgenerator.js
@@ -1,10 +1,12 @@
-// Variables initialization and assignation
-let firstName = prompt("Insert your first name");
-let lastName = prompt("Insert your last name");
-let favColor = prompt("Insert your favourite color");
-const notSoRandomNumber = 23;
+window.onload = function () {
+    // Variables initialization and assignation
+    let firstName = prompt("Insert your first name");
+    let lastName = prompt("Insert your last name");
+    let favColor = prompt("Insert your favourite color");
+    const notSoRandomNumber = 23;
 
-// Log and print
-console.log(`${firstName}${lastName}${favColor}${notSoRandomNumber}`)
+    // Log and print
+    console.log(`${firstName}${lastName}${favColor}${notSoRandomNumber}`)
 
-document.getElementById("password-container").innerHTML = `${firstName}${lastName}${notSoRandomNumber}`;
+    document.getElementById("password-container").innerHTML = `${firstName}${lastName}${notSoRandomNumber}`;
+}
